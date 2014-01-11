8c757675e9e9bf84fef5c10c28a482d627cae1dd
Alan v. Drake <alan.van.drake@gmail.com>
2014-01-11 15:30:34 +0100

    New tiles and map for testing water

--- maps/Main.tsx
+++ maps/Main.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Main" tilewidth="16" tileheight="16">
- <image source="../gfx/tileset.png" width="160" height="256"/>
+ <image source="../gfx/tileset.png" width="160" height="288"/>
  <tile id="23">
   <properties>
    <property name="type" value="block"/>
@@ -9,6 +9,16 @@
  <tile id="144">
   <properties>
    <property name="animation" value="3"/>
+  </properties>
+ </tile>
+ <tile id="165">
+  <properties>
+   <property name="animation" value="4"/>
+  </properties>
+ </tile>
+ <tile id="170">
+  <properties>
+   <property name="animation" value="4"/>
   </properties>
  </tile>
 </tileset>
